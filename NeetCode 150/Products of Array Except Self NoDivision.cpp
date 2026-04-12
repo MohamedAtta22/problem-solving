2b053b1760fd0efc064e4668df54554e9f937c9e
MohamedAtta22 <109322691+MohamedAtta22@users.noreply.github.com>
2026-04-13 00:01:28 +0200

    Refactor formatting and spacing in Products of Array Except Self solution

--- NeetCode 150/Products of Array Except Self NoDivision.cpp
+++ NeetCode 150/Products of Array Except Self NoDivision.cpp
@@ -1,17 +1,20 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int>result(nums.size(), 1);
+    vector<int> productExceptSelf(vector<int> &nums)
+    {
+        vector<int> result(nums.size(), 1);
         int prefix = 1;
         int suffix = 1;
-        for(int i = 0; i < nums.size(); ++i){
+        for (int i = 0; i < nums.size(); ++i)
+        {
             result[i] = prefix;
             prefix *= nums[i];
         }
-        for(int i = nums.size() - 1; i >= 0 ; --i){
+        for (int i = nums.size() - 1; i >= 0; --i)
+        {
             result[i] = suffix * result[i];
             suffix *= nums[i];
-
         }
         return result;
     }
