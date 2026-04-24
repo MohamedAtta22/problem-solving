a4d0eedebe0994919aa241e28e3acbff526404e1
MohamedAtta22 <109322691+MohamedAtta22@users.noreply.github.com>
2026-04-24 20:51:25 +0300

    ADD solution for Trapping Rain Water problem

---
+++ NeetCode 150/Trapping-Rain-Water.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l = 0, r = height.size()-1;
+        int lMax = 0, rMax = 0, res = 0;
+        while (l < r) {
+            lMax = max(lMax, height[l]);
+            rMax = max(rMax, height[r]);
+            if (lMax <= rMax) res += lMax - height[l++];
+            else res += rMax - height[r--];
+        }
+        return res;
+    }
+};
